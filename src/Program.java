46dc5a6fd8beab0c8540b2614b1fed4cf5a1e816
huynq <qhuydtvt@gmail.com>
2017-07-29 20:13:21 +0700

    Changed game window title

--- src/Program.java
+++ src/Program.java
@@ -6,7 +6,6 @@ import touhou.GameWindow;
 public class Program {
     public static void main(String[] args) {
         GameWindow gameWindow = new GameWindow();
-        System.out.println("Starting loop");
         gameWindow.loop();
     }
 }
